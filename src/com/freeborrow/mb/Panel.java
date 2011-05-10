74355828fc823e9ac2a8afd1e8645d1f18dbd7b7
Pete Barton <pete@thebartonfamily.com>
2011-05-09 23:18:53 -0600

    Change-Id: Ib9bf7eff77337b60cdc870a6387292a261bea977

---
+++ src/com/freeborrow/mb/Panel.java
@@ -0,0 +1,65 @@
+package com.freeborrow.mb;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class Panel extends SurfaceView implements SurfaceHolder.Callback {
+	private CanvasThread canvasthread;
+	
+	public Panel(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+		getHolder().addCallback(this);
+		canvasthread = new CanvasThread(getHolder(),this);
+		setFocusable(true);
+	}
+	
+	 public Panel(Context context) {
+		   super(context);
+		    getHolder().addCallback(this);
+		    canvasthread = new CanvasThread(getHolder(), this);
+		    setFocusable(true);
+
+	    }
+
+	 @Override
+	 public void onDraw(Canvas canvas) {
+		 Paint paint = new Paint();
+		 Bitmap kangoo = BitmapFactory.decodeResource(getResources(), R.drawable.kangoo);
+		 canvas.drawColor(Color.BLACK);
+		 canvas.drawBitmap(kangoo, 10, 10, null);
+	 }
+	 
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height){
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder){
+		canvasthread.setRunning(true);
+		canvasthread.start();
+	}
+	
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder){
+		boolean retry = true;
+		canvasthread.setRunning(false);
+		while (retry) {
+			try {
+				canvasthread.join();
+				retry = false;
+			} catch (InterruptedException e) {
+				//we will try again and again
+			}
+		}
+	}
+	
+}
